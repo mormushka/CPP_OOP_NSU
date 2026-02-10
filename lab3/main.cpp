20c6d65189c82a369b4a3e0c72aecf1ebfa5e45e
mormushka <roman_tikhonov_0_0@mail.ru>
2026-02-10 21:00:29 +0700

    CLI11 main

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <string>
+
 #include <CLI11.hpp>
 
-int main()
+int main(int argc, char *argv[])
 {
+    CLI::App app{"options"};
+
+    std::string config;
+    std::string output;
+    std::vector<std::string> inputs;
+
+    app.add_option("-c,--config", config, "Config file")->required();
+    app.add_option("-o,--output", output, "Output file")->required();
+    app.add_option("-i,--inputs", inputs, "Input files")->required();
+
+    try
+    {
+        app.parse(argc, argv);
+    }
+    catch (const CLI::ParseError &e)
+    {
+        return app.exit(e);
+    }
+
     return 0;
 }
